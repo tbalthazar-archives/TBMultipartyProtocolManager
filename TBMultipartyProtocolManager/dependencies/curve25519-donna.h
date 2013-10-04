0990d91204690df8b40bc82b7f37ce145724359e
Thomas Balthazar <thomas@balthazar.info>
2013-10-04 18:25:08 +0200

    don’t use static library for curve25519, add the source and create .h directly in the project

---
+++ TBMultipartyProtocolManager/dependencies/curve25519-donna.h
@@ -0,0 +1,9 @@
+//
+//  curve25519-donna.h
+//  TBMultipartyProtocolManager
+//
+//  Created by Thomas Balthazar on 04/10/13.
+//  Copyright (c) 2013 Thomas Balthazar. All rights reserved.
+//
+
+void curve25519_donna(uint8_t *, const uint8_t *, const uint8_t *);
