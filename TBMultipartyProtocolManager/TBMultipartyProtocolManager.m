ce8e035b822a2c7f068dce68cf6874328c0c5ae1
Thomas Balthazar <thomas@balthazar.info>
2013-10-04 16:15:10 +0200

    Generate private key (curve25519)

--- TBMultipartyProtocolManager/TBMultipartyProtocolManager.m
+++ TBMultipartyProtocolManager/TBMultipartyProtocolManager.m
@@ -7,7 +7,65 @@
 //
 
 #import "TBMultipartyProtocolManager.h"
+#import "NSData+Base64.h"
 
+// functions from curve25519-donna.a
+extern void curve25519_donna(uint8_t *, const uint8_t *, const uint8_t *);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+@interface TBMultipartyProtocolManager ()
+
+@property (nonatomic, strong, readwrite) NSString *privateKey;
+@property (nonatomic, strong, readwrite) NSString *publicKey;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
 @implementation TBMultipartyProtocolManager
+
+static TBMultipartyProtocolManager *sharedMultipartyProtocolManager = nil;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Initializer
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
++ (TBMultipartyProtocolManager *)sharedMultipartyProtocolManager {
+  if (sharedMultipartyProtocolManager==nil) {
+    sharedMultipartyProtocolManager = [[self alloc] init];
+  }
+  
+  return sharedMultipartyProtocolManager;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (id)init {
+  if (self=[super init]) {
+    // generate a private key (32 random bytes)
+    uint8_t private_key[32];
+    private_key[0] &= 248;
+    private_key[31] &= 127;
+    private_key[31] |= 64;
+    
+    // generate public key
+    uint8_t public_key[32];
+    static const uint8_t basepoint[32] = {9};
+    curve25519_donna(public_key, private_key, basepoint);
+    
+    
+    NSData *publicKeyData = [NSData dataWithBytes:public_key length:sizeof(public_key)];
+    NSData *privateKeyData = [NSData dataWithBytes:private_key length:sizeof(private_key)];
+    
+    _privateKey = [privateKeyData base64EncodedString];
+    _publicKey = [publicKeyData base64EncodedString];
+  }
+  
+  return self;
+}
 
 @end
