b0a949348fe045034e77df0ccd2a7f3579b59da2
Thomas Balthazar <thomas@balthazar.info>
2013-10-07 15:02:52 +0200

    JSON helper methods

---
+++ TBMultipartyProtocolManager/NSString+TBMultipartyProtocolManager.m
@@ -0,0 +1,40 @@
+//
+//  NSString+TBMultipartyProtocolManager.m
+//  TBMultipartyProtocolManager
+//
+//  Created by Thomas Balthazar on 07/10/13.
+//  Copyright (c) 2013 Thomas Balthazar. All rights reserved.
+//
+
+#import "NSString+TBMultipartyProtocolManager.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+@implementation NSString (TBMultipartyProtocolManager)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
++ (NSString *)tb_stringFromJSONObject:(id)JSONObject {
+  if (![NSJSONSerialization isValidJSONObject:JSONObject]) return nil;
+  
+  NSError *error;
+  NSData *JSONData = [NSJSONSerialization dataWithJSONObject:JSONObject options:0 error:&error];
+  if (error!=nil) return nil;
+  
+  return [[NSString alloc] initWithData:JSONData encoding:NSUTF8StringEncoding];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
++ (NSDictionary *)tb_JSONStringToDictionary:(NSString *)JSONString {
+  NSData *JSONData = [JSONString dataUsingEncoding:NSUTF8StringEncoding];
+  NSError *error;
+  NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:JSONData
+                                                             options:0
+                                                               error:&error];
+  if (error!=nil) return nil;
+  
+  return dictionary;
+}
+
+
+@end
