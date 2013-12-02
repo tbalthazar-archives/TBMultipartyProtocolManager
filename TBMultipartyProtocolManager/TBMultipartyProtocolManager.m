f8d8f5d35f547096ba7af8b873648e6b8033df20
Thomas Balthazar <thomas@balthazar.info>
2013-12-02 14:18:55 +0100

    Add License

--- TBMultipartyProtocolManager/TBMultipartyProtocolManager.m
+++ TBMultipartyProtocolManager/TBMultipartyProtocolManager.m
@@ -5,6 +5,21 @@
 //  Created by Thomas Balthazar on 04/10/13.
 //  Copyright (c) 2013 Thomas Balthazar. All rights reserved.
 //
+//  This file is part of TBMultipartyProtocolManager.
+//
+//  TBMultipartyProtocolManager is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  TBMultipartyProtocolManager is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with Cryptocat for iOS.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #import "TBMultipartyProtocolManager.h"
 #import "NSString+TBMultipartyProtocolManager.h"
@@ -542,7 +557,7 @@ void init_ctr(struct ctr_state *state, const unsigned char iv[12]) {
 //                                                       encoding:NSUTF8StringEncoding];
 //  // simulate missing recipient
 //  if ([debug_decryptedMsg isEqualToString:@"missingRecipients"]) {
-//    missingRecipients = @[@"a missing recipient", @"another one"];
+//    missingRecipients = @[@"john", @"paul", @"george", @"joe", @"lou"];
 //  }
 //  
 //  // simulate unreadable message
--- TBMultipartyProtocolManager/dependencies/include/curve25519/curve25519-donna.h
+++ TBMultipartyProtocolManager/dependencies/include/curve25519/curve25519-donna.h
@@ -5,5 +5,20 @@
 //  Created by Thomas Balthazar on 04/10/13.
 //  Copyright (c) 2013 Thomas Balthazar. All rights reserved.
 //
+//  This file is part of TBMultipartyProtocolManager.
+//
+//  TBMultipartyProtocolManager is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  TBMultipartyProtocolManager is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with Cryptocat for iOS.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 void curve25519_donna(uint8_t *, const uint8_t *, const uint8_t *);
